17fc6902cb64fd4d9d1dd279bbfa3e6b170e0dd6
andreabeta <mctech26@gmail.com>
2023-02-02 14:48:02 +0100

    Updated fix

--- fix-regex.sh
+++ fix-regex.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -path '*.rb' | xargs sed -i "s/Regexp\.new(.*?)(?:\\\\)+\//Regexp.new\1\\\/g"
+find . -type f -path '*.rb' | xargs sed -i -s -E "s/Regexp\.new(.*?)(\\\\)+\//Regexp.new\1\\//g"
