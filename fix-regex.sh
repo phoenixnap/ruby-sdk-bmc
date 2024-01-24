03014549ab8afb04b7302cece516a36c44c7b732
YandrickG <yandrickg@ccbill.com>
2024-01-24 15:07:26 +0100

    Testing regex

--- fix-regex.sh
+++ fix-regex.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo Entering $1...
 cd $1
-find . -type f -path '*.rb' | xargs sed -i -s -E "s/Regexp\.new(.*?)(\\\\)+\//Regexp.new\1\\//g"
+#find . -type f -path '*.rb' | xargs sed -i -s -E "s/Regexp\.new(.*?)(\\\\)+\//Regexp.new\1\\//g"
+#find . -type f -path '*.rb' | xargs sed -i -s -E "s/Regexp.new(\/^(\/[\w-]+)+$|^$\/)/Regexp.new(\/^(\\/[\w-]+)+$|^$\/)/g"
