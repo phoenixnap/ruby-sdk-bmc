d542e26478d569b2de8ffd58fea47a6fc4e9a018
YandrickG <yandrickg@ccbill.com>
2024-01-24 15:15:12 +0100

    Removing regex fix step

--- fix-regex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo Entering $1...
-cd $1
-#find . -type f -path '*.rb' | xargs sed -i -s -E "s/Regexp\.new(.*?)(\\\\)+\//Regexp.new\1\\//g"
-#find . -type f -path '*.rb' | xargs sed -i -s -E "s/Regexp.new(\/^(\/[\w-]+)+$|^$\/)/Regexp.new(\/^(\\/[\w-]+)+$|^$\/)/g"
